f8f05e593f0a5276b7fe5374e82a18fef4edff91
Petro Arva <petroarva33@gmail.com>
2022-01-27 14:24:43 +0100

    Added slider to the listing

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -6,14 +6,18 @@ import { db } from "../firebase.config";
 import Spinner from "../components/Spinner";
 import shareIcon from "../assets/svg/shareIcon.svg";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css";
 
 const Listing = () => {
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
   const [shareLinkCopied, setShareLinkCopied] = useState(false);
-  const navigate = useNavigate();
   const params = useParams();
   const auth = getAuth();
+
+  SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
   useEffect(() => {
     const fetchListing = async () => {
@@ -36,7 +40,19 @@ const Listing = () => {
   }
   return (
     <main>
-      {/* slider */}
+      <Swiper slidesPerView={1} pagination={{ clickable: true }}>
+        {listing.imgUrls.map((url, index) => (
+          <SwiperSlide key={index}>
+            <div
+              className="swiperSlideDiv"
+              style={{
+                background: `url(${listing.imgUrls[index]}) center no-repeat`,
+                backgroundSize: "cover",
+              }}
+            ></div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
 
       <div
         className="shareIconDiv"
